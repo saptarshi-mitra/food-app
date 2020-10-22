54bac64d262888fce5dee6cd1957dd9f0a35a9eb
Saptarshi <mitra.saptarshi97@gmail.com>
2020-10-22 11:00:12 +0530

    components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { SearchResultComponent } from './search-result/search-result.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'search/:name',
+    component: SearchResultComponent
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './home/search/search.component';
+import { TrendingComponent } from './home/trending/trending.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    TrendingComponent,
+    SearchResultComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
