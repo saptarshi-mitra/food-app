e03f2f3124db9dca739a0fe32b4afe0e4c5984ab
thenath1998 <rkz.suman@gmail.com>
2020-10-24 16:39:27 +0530

    footer and minor changes to trending

--- src/app/common/services/food.service.ts
+++ src/app/common/services/food.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +17,8 @@ export class FoodService {
 
   key_sumon = "51bcd51d265942b083eff5936d4e2ddb";
 
-  getRandomRecipe(){
-    return this.http.get<any>(` https://api.spoonacular.com/recipes/random?number=10&apiKey=${this.key_sumon}`)
+  getRandomRecipe(count){
+    return this._http.get<any>(` https://api.spoonacular.com/recipes/random?number=${count}&apiKey=${this.key_sumon}`)
   }
 
 }
