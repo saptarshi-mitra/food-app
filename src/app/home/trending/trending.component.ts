eff1bc276ec231d7a245edcd21c3af7789de1791
thenath1998 <rkz.suman@gmail.com>
2020-10-29 16:04:57 +0530

    carousel

--- src/app/home/trending/trending.component.ts
+++ src/app/home/trending/trending.component.ts
@@ -9,6 +9,7 @@ import { FoodService } from 'src/app/common/services/food.service';
 export class TrendingComponent implements OnInit {
 
   trending;
+  tempImage = "https://static.vecteezy.com/system/resources/previews/000/463/565/non_2x/healthy-food-clipart-vector.jpg";
   constructor(private foodService: FoodService) { }
 
   ngOnInit(): void {
