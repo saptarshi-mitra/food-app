58f0ccae4cd119c230c5add00c9fc9a18be3f35b
thenath1998 <rkz.suman@gmail.com>
2020-11-02 16:08:41 +0530

    light modifications

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,29 +7,24 @@ import { AuthService } from '../auth/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent implements OnInit {
 
   constructor(private authService: AuthService) { }
 
   isAuthenticated = false;
-  private userSub: Subscription;
-  userName;
+  userName:string;
 
 
   ngOnInit(): void {
     this.userName = "";
-    this.userSub = this.authService.user.subscribe(user => {
-      this.isAuthenticated = !!user;
+    this.authService.user.subscribe(user => {
       if (!!user) {
         this.authService.getDetails(user.id, user.token).subscribe(username => {
           this.userName = username.userName;
+          this.isAuthenticated = true;
         })
       }
     });
-  }
-
-  ngOnDestroy() {
-    this.userSub.unsubscribe();
   }
 
   onLogout() {
